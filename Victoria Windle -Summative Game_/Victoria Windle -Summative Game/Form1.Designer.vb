b9be44165e68b677c9e1ed0b2c74dd09349a38a2
victoriaWindle <>
2021-09-13 15:23:14 -0500

    added file

---
+++ Victoria Windle -Summative Game_/Victoria Windle -Summative Game/Form1.Designer.vb
@@ -0,0 +1,386 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.drop3 = New System.Windows.Forms.ComboBox()
+        Me.drop2 = New System.Windows.Forms.ComboBox()
+        Me.drop1 = New System.Windows.Forms.ComboBox()
+        Me.drop9 = New System.Windows.Forms.ComboBox()
+        Me.drop8 = New System.Windows.Forms.ComboBox()
+        Me.drop7 = New System.Windows.Forms.ComboBox()
+        Me.drop6 = New System.Windows.Forms.ComboBox()
+        Me.drop5 = New System.Windows.Forms.ComboBox()
+        Me.drop4 = New System.Windows.Forms.ComboBox()
+        Me.picBox8 = New System.Windows.Forms.PictureBox()
+        Me.picBox7 = New System.Windows.Forms.PictureBox()
+        Me.picBox9 = New System.Windows.Forms.PictureBox()
+        Me.picBox6 = New System.Windows.Forms.PictureBox()
+        Me.picBox5 = New System.Windows.Forms.PictureBox()
+        Me.picBox4 = New System.Windows.Forms.PictureBox()
+        Me.picBox1 = New System.Windows.Forms.PictureBox()
+        Me.picBox2 = New System.Windows.Forms.PictureBox()
+        Me.picBox3 = New System.Windows.Forms.PictureBox()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.lblOne = New System.Windows.Forms.Label()
+        Me.lblTwo = New System.Windows.Forms.Label()
+        Me.btn_Check = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        CType(Me.picBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(24, 393)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(279, 23)
+        Me.btnOne.TabIndex = 9
+        Me.btnOne.Text = "Press to randomize images"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(362, 12)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 10
+        Me.btnTwo.Text = "Exit"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'drop3
+        '
+        Me.drop3.Enabled = False
+        Me.drop3.FormattingEnabled = True
+        Me.drop3.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop3.Location = New System.Drawing.Point(214, 120)
+        Me.drop3.Name = "drop3"
+        Me.drop3.Size = New System.Drawing.Size(89, 21)
+        Me.drop3.TabIndex = 11
+        '
+        'drop2
+        '
+        Me.drop2.AllowDrop = True
+        Me.drop2.Enabled = False
+        Me.drop2.FormattingEnabled = True
+        Me.drop2.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop2.Location = New System.Drawing.Point(119, 120)
+        Me.drop2.MaxLength = 10
+        Me.drop2.Name = "drop2"
+        Me.drop2.Size = New System.Drawing.Size(89, 21)
+        Me.drop2.TabIndex = 12
+        '
+        'drop1
+        '
+        Me.drop1.Enabled = False
+        Me.drop1.FormattingEnabled = True
+        Me.drop1.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop1.Location = New System.Drawing.Point(24, 120)
+        Me.drop1.Name = "drop1"
+        Me.drop1.Size = New System.Drawing.Size(89, 21)
+        Me.drop1.TabIndex = 13
+        '
+        'drop9
+        '
+        Me.drop9.Enabled = False
+        Me.drop9.FormattingEnabled = True
+        Me.drop9.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop9.Location = New System.Drawing.Point(214, 366)
+        Me.drop9.Name = "drop9"
+        Me.drop9.Size = New System.Drawing.Size(89, 21)
+        Me.drop9.TabIndex = 14
+        '
+        'drop8
+        '
+        Me.drop8.Enabled = False
+        Me.drop8.FormattingEnabled = True
+        Me.drop8.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop8.Location = New System.Drawing.Point(119, 366)
+        Me.drop8.Name = "drop8"
+        Me.drop8.Size = New System.Drawing.Size(89, 21)
+        Me.drop8.TabIndex = 15
+        '
+        'drop7
+        '
+        Me.drop7.Enabled = False
+        Me.drop7.FormattingEnabled = True
+        Me.drop7.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop7.Location = New System.Drawing.Point(24, 366)
+        Me.drop7.Name = "drop7"
+        Me.drop7.Size = New System.Drawing.Size(89, 21)
+        Me.drop7.TabIndex = 16
+        '
+        'drop6
+        '
+        Me.drop6.Enabled = False
+        Me.drop6.FormattingEnabled = True
+        Me.drop6.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop6.Location = New System.Drawing.Point(214, 242)
+        Me.drop6.Name = "drop6"
+        Me.drop6.Size = New System.Drawing.Size(89, 21)
+        Me.drop6.TabIndex = 17
+        '
+        'drop5
+        '
+        Me.drop5.Enabled = False
+        Me.drop5.FormattingEnabled = True
+        Me.drop5.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop5.Location = New System.Drawing.Point(119, 242)
+        Me.drop5.Name = "drop5"
+        Me.drop5.Size = New System.Drawing.Size(89, 21)
+        Me.drop5.TabIndex = 18
+        '
+        'drop4
+        '
+        Me.drop4.Enabled = False
+        Me.drop4.FormattingEnabled = True
+        Me.drop4.Items.AddRange(New Object() {"Frog", "Pencil", "Pumpkin", "Apple", "Tree", "Books", "Panda", "Cat", "Fork", "Duck"})
+        Me.drop4.Location = New System.Drawing.Point(24, 242)
+        Me.drop4.Name = "drop4"
+        Me.drop4.Size = New System.Drawing.Size(89, 21)
+        Me.drop4.TabIndex = 19
+        '
+        'picBox8
+        '
+        Me.picBox8.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox8.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox8.Location = New System.Drawing.Point(119, 269)
+        Me.picBox8.Name = "picBox8"
+        Me.picBox8.Size = New System.Drawing.Size(89, 91)
+        Me.picBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox8.TabIndex = 8
+        Me.picBox8.TabStop = False
+        '
+        'picBox7
+        '
+        Me.picBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox7.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox7.Location = New System.Drawing.Point(24, 269)
+        Me.picBox7.Name = "picBox7"
+        Me.picBox7.Size = New System.Drawing.Size(89, 91)
+        Me.picBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox7.TabIndex = 7
+        Me.picBox7.TabStop = False
+        '
+        'picBox9
+        '
+        Me.picBox9.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox9.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox9.Location = New System.Drawing.Point(214, 269)
+        Me.picBox9.Name = "picBox9"
+        Me.picBox9.Size = New System.Drawing.Size(89, 91)
+        Me.picBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox9.TabIndex = 6
+        Me.picBox9.TabStop = False
+        '
+        'picBox6
+        '
+        Me.picBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox6.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox6.Location = New System.Drawing.Point(214, 147)
+        Me.picBox6.Name = "picBox6"
+        Me.picBox6.Size = New System.Drawing.Size(89, 91)
+        Me.picBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox6.TabIndex = 5
+        Me.picBox6.TabStop = False
+        '
+        'picBox5
+        '
+        Me.picBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox5.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox5.Location = New System.Drawing.Point(119, 147)
+        Me.picBox5.Name = "picBox5"
+        Me.picBox5.Size = New System.Drawing.Size(89, 91)
+        Me.picBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox5.TabIndex = 4
+        Me.picBox5.TabStop = False
+        '
+        'picBox4
+        '
+        Me.picBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox4.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox4.Location = New System.Drawing.Point(24, 147)
+        Me.picBox4.Name = "picBox4"
+        Me.picBox4.Size = New System.Drawing.Size(89, 91)
+        Me.picBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox4.TabIndex = 3
+        Me.picBox4.TabStop = False
+        '
+        'picBox1
+        '
+        Me.picBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox1.BackgroundImage = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.frog_157934_1280
+        Me.picBox1.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox1.Location = New System.Drawing.Point(24, 23)
+        Me.picBox1.Name = "picBox1"
+        Me.picBox1.Size = New System.Drawing.Size(89, 91)
+        Me.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox1.TabIndex = 2
+        Me.picBox1.TabStop = False
+        '
+        'picBox2
+        '
+        Me.picBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox2.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox2.Location = New System.Drawing.Point(119, 23)
+        Me.picBox2.Name = "picBox2"
+        Me.picBox2.Size = New System.Drawing.Size(89, 91)
+        Me.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox2.TabIndex = 1
+        Me.picBox2.TabStop = False
+        '
+        'picBox3
+        '
+        Me.picBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.picBox3.Image = Global.Victoria_Windle__Summative_Game.My.Resources.Resources.Design_2
+        Me.picBox3.Location = New System.Drawing.Point(214, 23)
+        Me.picBox3.Name = "picBox3"
+        Me.picBox3.Size = New System.Drawing.Size(89, 91)
+        Me.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picBox3.TabIndex = 0
+        Me.picBox3.TabStop = False
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(165, 422)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(138, 23)
+        Me.btn3.TabIndex = 20
+        Me.btn3.Text = "Press to start"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'lblOne
+        '
+        Me.lblOne.AutoSize = True
+        Me.lblOne.Location = New System.Drawing.Point(12, 428)
+        Me.lblOne.Name = "lblOne"
+        Me.lblOne.Size = New System.Drawing.Size(39, 13)
+        Me.lblOne.TabIndex = 21
+        Me.lblOne.Text = "Points:"
+        '
+        'lblTwo
+        '
+        Me.lblTwo.AutoSize = True
+        Me.lblTwo.Location = New System.Drawing.Point(57, 428)
+        Me.lblTwo.Name = "lblTwo"
+        Me.lblTwo.Size = New System.Drawing.Size(0, 13)
+        Me.lblTwo.TabIndex = 22
+        '
+        'btn_Check
+        '
+        Me.btn_Check.Location = New System.Drawing.Point(309, 422)
+        Me.btn_Check.Name = "btn_Check"
+        Me.btn_Check.Size = New System.Drawing.Size(128, 23)
+        Me.btn_Check.TabIndex = 23
+        Me.btn_Check.Text = "Check my Answers!"
+        Me.btn_Check.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(322, 41)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(127, 257)
+        Me.RichTextBox1.TabIndex = 24
+        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.btn_Check)
+        Me.Controls.Add(Me.lblTwo)
+        Me.Controls.Add(Me.lblOne)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.drop4)
+        Me.Controls.Add(Me.drop5)
+        Me.Controls.Add(Me.drop6)
+        Me.Controls.Add(Me.drop7)
+        Me.Controls.Add(Me.drop8)
+        Me.Controls.Add(Me.drop9)
+        Me.Controls.Add(Me.drop1)
+        Me.Controls.Add(Me.drop2)
+        Me.Controls.Add(Me.drop3)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Controls.Add(Me.picBox8)
+        Me.Controls.Add(Me.picBox7)
+        Me.Controls.Add(Me.picBox9)
+        Me.Controls.Add(Me.picBox6)
+        Me.Controls.Add(Me.picBox5)
+        Me.Controls.Add(Me.picBox4)
+        Me.Controls.Add(Me.picBox1)
+        Me.Controls.Add(Me.picBox2)
+        Me.Controls.Add(Me.picBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBox3 As PictureBox
+    Friend WithEvents picBox2 As PictureBox
+    Friend WithEvents picBox1 As PictureBox
+    Friend WithEvents picBox4 As PictureBox
+    Friend WithEvents picBox5 As PictureBox
+    Friend WithEvents picBox6 As PictureBox
+    Friend WithEvents picBox9 As PictureBox
+    Friend WithEvents picBox7 As PictureBox
+    Friend WithEvents picBox8 As PictureBox
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents drop3 As ComboBox
+    Friend WithEvents drop2 As ComboBox
+    Friend WithEvents drop1 As ComboBox
+    Friend WithEvents drop9 As ComboBox
+    Friend WithEvents drop8 As ComboBox
+    Friend WithEvents drop7 As ComboBox
+    Friend WithEvents drop6 As ComboBox
+    Friend WithEvents drop5 As ComboBox
+    Friend WithEvents drop4 As ComboBox
+    Friend WithEvents btn3 As Button
+    Friend WithEvents lblOne As Label
+    Friend WithEvents lblTwo As Label
+    Friend WithEvents btn_Check As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Victoria Windle -Summative Game_/Victoria Windle -Summative Game/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+
+    Dim images() As Image = {My.Resources.Design_2, My.Resources.frog, My.Resources.Pencil, My.Resources.Pumpkin, My.Resources.Apple, My.Resources.tree, My.Resources.Books, My.Resources.Panda, My.Resources.Cat, My.Resources.Fork, My.Resources.Duck}
+    'Dim picBoxes() As PictureBox = {picBox1, picBox2, picBox3, picBox4, picBox5, picBox6, picBox7, picBox8, picBox9}
+    Dim value, value2, value3, value4, value5, value6, value7, value8, value9 As Integer
+    Dim points, hi As Integer
+
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        randomizeImage()
+
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        'Would have liked to use a for each statement here...
+
+        picBox1.Image = My.Resources.Design_2
+        picBox2.Image = My.Resources.Design_2
+        picBox3.Image = My.Resources.Design_2
+        picBox4.Image = My.Resources.Design_2
+        picBox5.Image = My.Resources.Design_2
+        picBox6.Image = My.Resources.Design_2
+        picBox7.Image = My.Resources.Design_2
+        picBox8.Image = My.Resources.Design_2
+        picBox9.Image = My.Resources.Design_2
+
+        drop1.Enabled = True
+        drop2.Enabled = True
+        drop3.Enabled = True
+        drop4.Enabled = True
+        drop5.Enabled = True
+        drop6.Enabled = True
+        drop7.Enabled = True
+        drop8.Enabled = True
+        drop9.Enabled = True
+
+        btnOne.Enabled = False
+        btn3.Enabled = False
+    End Sub
+
+    Private Function randomizeImage()
+        '...Or a for each statement here
+        'there are 10 images because I like even numbers, the pictures do not come out completely random
+        'I don't know why.
+        value = CInt(Int((10 * Rnd()) + 1))
+        picBox1.Image = images(value)
+
+        value2 = CInt(Int((10 * Rnd()) + 1))
+        picBox2.Image = images(value2)
+
+        value3 = CInt(Int((10 * Rnd()) + 1))
+        picBox3.Image = images(value3)
+
+        value4 = CInt(Int((10 * Rnd()) + 1))
+        picBox4.Image = images(value4)
+
+        value5 = CInt(Int((10 * Rnd()) + 1))
+        picBox5.Image = images(value5)
+
+        value6 = CInt(Int((10 * Rnd()) + 1))
+        picBox6.Image = images(value6)
+
+        value7 = CInt(Int((10 * Rnd()) + 1))
+        picBox7.Image = images(value7)
+
+        value8 = CInt(Int((10 * Rnd()) + 1))
+        picBox8.Image = images(value8)
+
+        value9 = CInt(Int((10 * Rnd()) + 1))
+        picBox9.Image = images(value9)
+
+
+        Return (value, value2, value3, value4, value5, value6, value7, value8, value9)
+
+    End Function
+
+
+
+    Private Sub btn_Check_Click(sender As Object, e As EventArgs) Handles btn_Check.Click
+
+        If drop1.SelectedIndex + 1 = value Then
+            drop1.Enabled = False
+            points = points + 10
+        End If
+
+        If drop2.SelectedIndex + 1 = value2 Then
+            drop2.Enabled = False
+            points = points + 10
+        End If
+
+        If drop3.SelectedIndex + 1 = value3 Then
+            drop3.Enabled = False
+            points = points + 10
+        End If
+
+        If drop4.SelectedIndex + 1 = value4 Then
+            drop4.Enabled = False
+            points = points + 10
+        End If
+
+        If drop5.SelectedIndex + 1 = value5 Then
+            drop5.Enabled = False
+            points = points + 10
+        End If
+
+        If drop6.SelectedIndex + 1 = value6 Then
+            drop6.Enabled = False
+            points = points + 10
+        End If
+
+        If drop7.SelectedIndex + 1 = value7 Then
+            drop7.Enabled = False
+            points = points + 10
+        End If
+
+        If drop8.SelectedIndex + 1 = value8 Then
+            drop8.Enabled = False
+            points = points + 10
+        End If
+
+        If drop9.SelectedIndex + 1 = value9 Then
+            drop9.Enabled = False
+            points = points + 10
+        End If
+
+        MessageBox.Show("you have " & points & " points")
+
+
+
+        RefreshForm()
+
+        lblTwo.Text = points
+        If points >= 30 Then
+            MessageBox.Show("30 pts: Fun fact: Crows have been observed using tools and can talk.")
+        End If
+        If points >= 50 Then
+            MessageBox.Show("50 pts: Jesus is awesome!")
+        End If
+        If points >= 100 Then
+            prize100()
+        End If
+        If points = 150 Then
+            prize150()
+        End If
+
+    End Sub
+
+    Private Sub RefreshForm()
+        Me.Controls.Clear()
+        Me.InitializeComponent()
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        Close()
+    End Sub
+
+    'just cause I couldn't figure out how to use either in my actual code so here is proof I can do them as prizes lol
+    Private Sub prize100()
+        For hi = 0 To 10
+            MessageBox.Show("100 pts: " & hi)
+        Next
+    End Sub
+    Private Sub prize150()
+
+        hi = 3
+        Do Until hi = 6
+            points = points + 1
+            MessageBox.Show("150 pts: " & points)
+        Loop
+
+    End Sub
+
+End Class
